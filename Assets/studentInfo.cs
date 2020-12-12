de652fd44d7992b64dbd5007d3337a0d5b6f96a6
Ivan Kravchenko <ivan.kravchenko@georgebrown.ca>
2020-12-11 22:09:13 -0500

    student info in the title screen

---
+++ Assets/studentInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class studentInfo : MonoBehaviour
+{
+    [SerializeField]
+    public Text Chris;
+    [SerializeField]
+    public Text Ivan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Chris.text = "Beining Liu 101193350";
+        Ivan.text = "Ivan Kravchenko 101183016";
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
